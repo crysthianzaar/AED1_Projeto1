03ab56ef20bfa0e84df08c2876ac2c87d091b9c6
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-16 11:15:47 -0300

    Update usuario.cs

--- usuario.cs
+++ usuario.cs
@@ -1,7 +1,7 @@
 class usuario
 {
   public int idade;
-  public string nome;
+  public string nome= "ZAE";
   public string cpf;
   public string Perfil_Risco;
 
@@ -9,11 +9,17 @@ class usuario
  
   }
 
-  public void criarconta() {
- 
+  public void criarconta(string nome, string cpf, int idade) {
+  this.nome= nome;
+  this.cpf= cpf;
+  this.idade= idade;
   }
 
   public void novoteste_de_perfil() {
  
   }
+
+
+
+
 }
