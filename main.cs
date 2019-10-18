9d3af203a6398303ee51710d05fa4af9ee50a1e9
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-18 20:04:46 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,4 +1,3 @@
-  
 using System;
 
 class MainClass {
@@ -6,13 +5,28 @@ class MainClass {
     string criar;
     string possui;
     usuario c = new usuario();
+    carteira b = new carteira ();
+    investimentos a = new investimentos();
+    login d = new login();
+
+  
 
     Console.WriteLine(" Você já possui uma conta? (s = sim, n = Não)");
     possui = Console.ReadLine();
-      if(possui == "n"){
+    if(possui == "s" || possui == "S"){
+      Console.WriteLine(" Digite sua senha:");
+     string senha = Console.ReadLine();
+      if(d.validaSenha(senha) == true){
 
-    Console.WriteLine(" Deseja criar uma conta? (s = sim, n = Não)");
-    criar= Console.ReadLine();
+     Console.WriteLine("Aperte qualquer tecla para continuar");
+     Console.ReadKey();
+     a.Educacao_Financeira(b);
+  }
+    }
+   if(possui == "n" || possui == "N"){
+
+      Console.WriteLine(" Deseja criar uma conta? (s = sim, n = Não)");
+      criar= Console.ReadLine();
       if ( criar =="s"){
       c.criarconta();
       }
@@ -20,16 +34,17 @@ class MainClass {
         Console.WriteLine(" Espero que mude de ideia em breve! :)");
        Environment.Exit(0);
       }
+       c.SetAnalise_de_perfil();
+    Console.WriteLine();
+    Console.WriteLine("AGORA TEMOS TUDO QUE É PRECISO PARA COMEÇAR!");
 
-      
-    
-    c.analise_de_perfil();
-
-     
-
-   }  
+    Console.WriteLine("Aperte qualquer tecla para continuar");
+    Console.ReadKey();
+    b.SetSaldo_inicial();
+      }
   }
-
 }
+
+
 
 // Em Teste
