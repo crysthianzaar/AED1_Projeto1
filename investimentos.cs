bb5a77154f45b832717251aff510d041557acb33
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-18 20:05:40 -0300

    Update investimentos.cs

--- investimentos.cs
+++ investimentos.cs
@@ -1,9 +1,33 @@
+using System;
 class investimentos
 {
-  public string ativos_fisicos;
-  public string tesouro_direto;
-  public string acoes;
-  public string poupanca;
-  public string cdb;
+  public string ativos_fisicos = "";
+  public string tesouro_direto= "";
+  public string acoes= "";
+  public string poupanca= "";
 
+  public void Educacao_Financeira(carteira b) {
+    if (b.GetSaldo_inicial() <= 0){
+      Console.WriteLine();
+      Console.WriteLine("OK, não tem problema!");
+      Console.WriteLine("Podemos te explicar um pouco sobre finanças, o que acha?");
+      Console.WriteLine("1 - Opa! quero aprender!");
+      Console.WriteLine("2 - Melhor deixar para depois");
+      string escolha = Console.ReadLine();
+        if(escolha == "1"){
+          // criar texto para educação financeira
+        }
+        if(escolha == "2"){
+          // voltar para o menu depois de entrar
+        }
+      
+
+
+
+    }
+  }
+
+
+
+  
 }
