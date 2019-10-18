c1de8f081cb3efaf962b1dabbccc922c68f0eadf
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-18 20:05:19 -0300

    Update carteira.cs

--- carteira.cs
+++ carteira.cs
@@ -1,24 +1,23 @@
+using System;
 class carteira
 {
-  public double lucro_prejuizo;
-  public double rentabilidade;
-  public double saldo_inicial;
-  public string instituicao_financeira;
-public void setLucro(double novolucro  ){
-  lucroprejuizo=novolucro;
-}
- private double setSaldo(double novosaldo){
-  saldo_inicial=novosaldo;
+  public double lucro_prejuizov= 0;
+  public double rentabilidade = 0;
+  private double saldo_inicial = 0;
+  public string saldo = "";
 
- public string getBanco(){
-  return instituicao_financeira;
- }
-   public void inserirsaldo() {
+  public void SetSaldo_inicial() {
   Console.WriteLine("Quanto de saldo disponível você tem para investir?");
-  double sd_disponivel = double.Parse(Console.ReadLine());
- }
-public carteira(double lucro_prejuizo,double rentabilidade,double saldo_inicial){
- this.lucro_prejuizo=lucro_prejuizo;
-  this.saldo_inicial=saldo_inicial;
-  this.instituicao_financeira=instituicao_financeira;
+  saldo_inicial = double.Parse(Console.ReadLine());
+  }
+  public double GetSaldo_inicial(){
+    return saldo_inicial;
+  }
+
+
+
+
+  
+
+
 }
