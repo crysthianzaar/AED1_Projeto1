9e7d8cae89448e67dd639e01954e9cc55dbc7ec6
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-19 16:00:33 -0300

    Update investimentos.cs

--- investimentos.cs
+++ investimentos.cs
@@ -1,33 +1,93 @@
 using System;
+using System.IO;  
+
 class investimentos
 {
-  public string ativos_fisicos = "";
-  public string tesouro_direto= "";
-  public string acoes= "";
-  public string poupanca= "";
+  public double ativos_fisicos;
+  public string tesouro_direto;
+  public string acoes;
+  public string poupanca;
 
-  public void Educacao_Financeira(carteira b) {
+  public void Recomendacao_Inicial(carteira b) {
     if (b.GetSaldo_inicial() <= 0){
       Console.WriteLine();
       Console.WriteLine("OK, não tem problema!");
       Console.WriteLine("Podemos te explicar um pouco sobre finanças, o que acha?");
-      Console.WriteLine("1 - Opa! quero aprender!");
-      Console.WriteLine("2 - Melhor deixar para depois");
-      string escolha = Console.ReadLine();
-        if(escolha == "1"){
-          // criar texto para educação financeira
-        }
-        if(escolha == "2"){
-          // voltar para o menu depois de entrar
-        }
-      
-
-
-
+      Console.WriteLine();
+      Console.WriteLine("No Menu Principal você pode escolher a opção EDUCAÇÃO FINANCEIRA");
+      Console.WriteLine();
+      Console.WriteLine();
+      Menu_De_Investimentos();
+    }
+    if (b.GetSaldo_inicial() > 0){
+      Menu_De_Investimentos();
     }
   }
 
+  public void Menu_De_Investimentos(){
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine();
+    Console.WriteLine("1 - Ativos físicos");
+    Console.WriteLine("2 - Tesouro direto");
+    Console.WriteLine("3 - Ações");
+    Console.WriteLine("4 - Poupança");
+    Console.WriteLine("5 - IR PARA ABA DE EDUCAÇÃO FINANCEIRA");
+    Console.WriteLine("6 - Fazer SIMULAÇÕS de investimentos");
+    string lerresposta = Console.ReadLine();
 
+      if (lerresposta == "1"){
+        AtivosFisicos();
+      }
+      if (lerresposta == "2"){
+        Tesouro();
+      }
+      if (lerresposta == "3"){
+        SetAcoes();
+      }
+      if (lerresposta == "5"){
+        Educacao();
+      }
+      if (lerresposta == "6"){
+        Simulacao();
+      }
+  }
 
+  public void Educacao(){
+   Console.WriteLine("calma, ja te ajudo.");
+  }
+
+  public void Tesouro(){
+   Console.WriteLine("calma, ja te ajudo.");
+  }
+
+  public void SetAcoes(){
+   Console.WriteLine("calma, ja te ajudo.");
+  }
+
+  public void AtivosFisicos(){
+    Console.WriteLine("Escolha uma Opção:");
+    Console.WriteLine();
+    Console.WriteLine("1 - Adicionar um investimentos em   ativos físicos");
+    Console.WriteLine("2 - Explicação e fatos relevantes sobre Ativos Fisicos");
+    string ler1 = Console.ReadLine();
+    if(ler1 == "1"){
+    Console.WriteLine("Digite o valor que queira adicionar");
+    double add1 = double.Parse(Console.ReadLine());
+    ativos_fisicos = ativos_fisicos+add1;
+    }
+    if(ler1 == "2"){
+    // EM CONSTRUÇÃO
+    
+    }
+  }
+
+  public void Simulacao(){
+   Console.WriteLine("calma, ja te ajudo.");
+   }
   
+
+
+
+
+
 }
