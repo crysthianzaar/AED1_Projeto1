533ae242a7ffd8de4ed03c1de552e38126baacdc
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-22 01:45:21 -0300

    Adicionado novos modelos de investimentos

--- investimentos.cs
+++ investimentos.cs
@@ -82,7 +82,39 @@ class investimentos
   }
 
   public void Simulacao(){
-   Console.WriteLine("calma, ja te ajudo.");
+   Console.WriteLine("Digite o valor que você quer fazer a simulação:");
+   double valor = double.Parse(Console.ReadLine());
+   Console.WriteLine("Por quanto tempo ( EM MESES ) você pretende deixar o seu dinheiro investido");
+   double meses = double.Parse(Console.ReadLine());
+   Console.WriteLine("Qual o tipo de investimento você deseja? ");
+   Console.WriteLine("1 - Poupança");
+   Console.WriteLine("2 - CDB");
+   Console.WriteLine("3 - Tesouro Direto");
+   Console.WriteLine("4 - Ações/ FII's");
+   string escolha = Console.ReadLine();
+    if(escolha == "1"){
+      double result = (0.0037*valor*meses);
+      Console.WriteLine("O SEU RENDIMENTO NA POUPANÇA SERA: "+ result + " REAIS!");
+      // Com base na TAXA SELIC do dia 21/10/2019
+    }
+    if(escolha == "2"){
+      double result = 0.00458*meses*valor;
+      Console.WriteLine("O SEU RENDIMENTO NA SERA: "+ result + " REAIS!");
+      //Com base na TAXA SELIC e CDI do dia 21/10/2019
+
+    }
+    if(escolha == "3"){
+      Console.WriteLine("calma");
+
+    }
+    if(escolha == "4"){
+      Console.WriteLine("calma");
+
+    }
+
+
+
+
    }
   
 
