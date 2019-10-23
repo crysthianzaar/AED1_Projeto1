0c9624afc0442e7fe66c193f8067fdb9cbb4b0d5
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2019-10-23 01:20:39 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,17 +1,18 @@
+using System.IO;  
 using System;
+using System.Collections.Generic;
 
 class MainClass {
   public static void Main (string[] args) {
-    string criar;
-    string possui;
-    usuario c = new usuario();
-    carteira b = new carteira ();
-    investimentos a = new investimentos();
-    login d = new login();
+  string criar;
+  string possui;
 
-  
+  investimentos a = new investimentos();
+  carteira b = new carteira ();
+  usuario c = new usuario();
+  login d = new login();
 
-    Console.WriteLine(" Você já possui uma conta? (s = sim, n = Não)");
+  Console.WriteLine(" Você já possui uma conta? (s = sim, n = Não)");
     possui = Console.ReadLine();
     if(possui == "s" || possui == "S"){
       Console.WriteLine(" Digite sua senha:");
@@ -20,7 +21,7 @@ class MainClass {
 
      Console.WriteLine("Aperte qualquer tecla para continuar");
      Console.ReadKey();
-     a.Educacao_Financeira(b);
+     a.Menu_De_Investimentos();
   }
     }
    if(possui == "n" || possui == "N"){
@@ -41,10 +42,11 @@ class MainClass {
     Console.WriteLine("Aperte qualquer tecla para continuar");
     Console.ReadKey();
     b.SetSaldo_inicial();
+    a.Recomendacao_Inicial(b);
       }
   }
 }
 
 
 
-// Em Desenvolvimento
+// Em Teste
